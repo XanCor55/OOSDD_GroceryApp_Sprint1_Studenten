801cced9a90c238acf8ac21f526335b952ba737c
Xander <s1218452@student.windesheim.nl>
2025-09-12 13:42:06 +0200

    Assign ID values to the properties

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,12 +2,13 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId { get; set; }  //To do: change it to a property
-        public int ProductId { get; set; }      //To do: change it to a property
+        public int GroceryListId { get; set; }  //To do: change it to a property | Done
+        public int ProductId { get; set; }      //To do: change it to a property | Done
         public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            //To do:assign values to the properties
+            GroceryListItem item = GroceryListId = { GroceryListId = groceryListId }; //To do:assign values to the properties | Done
+            GroceryListItem item = ProductId = {ProductId = productId };
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
