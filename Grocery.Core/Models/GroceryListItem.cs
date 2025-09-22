b934d6957801b00fd400aaa38b88d91004bba8c1
Xander <s1218452@student.windesheim.nl>
2025-09-22 08:18:30 +0200

    Fix commit, perongeluk gepushed naar main inplaats naar develop dus heb hier die code aangepast.

--- Grocery.Core/Models/GroceryListItem.cs
+++ Grocery.Core/Models/GroceryListItem.cs
@@ -2,13 +2,13 @@
 {
     public class GroceryListItem : Model
     {
-        public int GroceryListId { get; set; }  //To do: change it to a property | Done
-        public int ProductId { get; set; }      //To do: change it to a property | Done
+        public int GroceryListId { get; set; }  
+        public int ProductId { get; set; }      
         public int Amount { get; set; }
         public GroceryListItem(int id, int groceryListId, int productId, int amount) : base(id, "")
         {
-            GroceryListItem item = GroceryListId = { GroceryListId = groceryListId }; //To do:assign values to the properties | Done
-            GroceryListItem item = ProductId = {ProductId = productId };
+            GroceryListId = groceryListId;
+            ProductId = ProductId;
         }
 
         public Product Product { get; set; } = new(0, "None", 0);
